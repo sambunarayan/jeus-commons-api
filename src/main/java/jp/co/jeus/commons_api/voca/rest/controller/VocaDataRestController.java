81c88ad28a04ab1d7f1b2bd1b41f5683da05e8aa
Jaewoo Kim <soyou30257@gmail.com>
2023-05-12 22:46:36 +0900

    Fixed

--- src/main/java/jp/co/jeus/commons_api/voca/rest/controller/VocaDataRestController.java
+++ src/main/java/jp/co/jeus/commons_api/voca/rest/controller/VocaDataRestController.java
@@ -1,6 +1,5 @@
 package jp.co.jeus.commons_api.voca.rest.controller;
 
-import jp.co.jeus.commons_api.commons.repository.entity.Vocabulary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
