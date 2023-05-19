689fafbd807d9aa758997639b3c1ae9914d6c96b
Jaewoo Kim <soyou30257@gmail.com>
2023-05-19 21:07:24 +0900

    Add registration controller

--- src/main/java/jp/co/jeus/commons_api/voca/rest/controller/VocaDataRestController.java
+++ src/main/java/jp/co/jeus/commons_api/voca/rest/controller/VocaDataRestController.java
@@ -4,6 +4,7 @@ import jp.co.jeus.commons_api.voca.dto.VocaSearchResponseDto;
 import jp.co.jeus.commons_api.voca.service.VocabularyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class VocaDataRestController {
     @GetMapping("find-all")
     public VocaSearchResponseDto findAllVocabulary() {
         return vocabularyService.findAll();
+    }
+
+    @PostMapping("add")
+    public void add() {
+
     }
 }
