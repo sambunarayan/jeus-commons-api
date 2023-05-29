3192037d188364b0a066a42d61f08a3d3836e8e6
Jaewoo Kim <soyou30257@gmail.com>
2023-05-29 21:52:01 +0900

    Add comments

--- src/main/java/jp/co/jeus/commons_api/commons/constants/Result.java
+++ src/main/java/jp/co/jeus/commons_api/commons/constants/Result.java
@@ -2,15 +2,15 @@ package jp.co.jeus.commons_api.commons.constants;
 
 public enum Result {
     /**
-     *
+     * Success
      */
     SUCCESS(0, "Success", "Success"),
     /**
-     *
+     * No contents
      */
     EMPTY(1, "Empty", "No contents"),
     /**
-     *
+     * Internal server error
      */
     INTERNAL_SERVER_ERROR(99, "Internal server error", "Unexpected error has occurred");
 
@@ -18,7 +18,7 @@ public enum Result {
     private final String name;
     private final String details;
 
-    private Result(int code, String name, String details) {
+    Result(int code, String name, String details) {
         this.code = code;
         this.name = name;
         this.details = details;
